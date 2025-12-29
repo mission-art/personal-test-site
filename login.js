ae9497d2fe715210caed47cf4c498ac236021da0
mission-art <mission@gwmek.org>
2025-12-29 08:19:11 -0600

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ function sleep(ms) {
 }
 let currentSteo = "askUsername";
 let username = "";
-submitBtn.addEventListener("click", async () => {
+submitBtn.addEventListener("click", async() => {
   const input = userInput.value.trim();
   userInput.value = "";
 
