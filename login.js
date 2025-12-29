eb4f444647521f4d62b99bcb10641e41e798e283
mission-art <mission@gwmek.org>
2025-12-29 10:36:28 -0600

    Update login.js

--- login.js
+++ login.js
@@ -37,7 +37,7 @@ submitBtn.addEventListener("click", async () => {
 
     // STEP 2 → Ask password
 } else if (currentStep === "askPassword") {
-    const password1 = input;
+    const password1 = input type="password";
 
     if (username === "student" && password1 === "123456") {
         print("You are logged in as a student.");
